c0f6dc0d666d433e28466917f2321a6bb944e239
Moklesur Rahman <moklesur.rahman@vegans.it>
2023-10-09 21:56:31 +0600

    domain repository added

---
+++ app/src/main/java/com/mmrbd/starwarsexplorer/domain/repositories/StarWarsRepository.kt
@@ -0,0 +1,14 @@
+package com.mmrbd.starwarsexplorer.domain.repositories
+
+import com.mmrbd.starwarsexplorer.domain.entities.CharacterEntity
+import com.mmrbd.starwarsexplorer.domain.entities.PlanetEntity
+import com.mmrbd.starwarsexplorer.domain.entities.StarshipEntity
+import com.mmrbd.starwarsexplorer.utils.Result
+import kotlinx.coroutines.flow.Flow
+
+interface StarWarsRepository {
+     fun getCharacters(): Flow<Result<List<CharacterEntity>>>
+    fun getCharacterDetails(id: Int): Flow<CharacterEntity>
+    fun getPlanets(): Flow<Result<List<PlanetEntity>>>
+    fun getStarships(): Flow<Result<List<StarshipEntity>>>
+}
