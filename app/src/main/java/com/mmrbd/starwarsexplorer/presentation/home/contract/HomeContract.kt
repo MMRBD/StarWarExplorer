af1d24b70ece82e35d6ee7b6c00b4768a8c86092
Moklesur Rahman <moklesur.rahman@vegans.it>
2023-10-10 17:45:10 +0600

    character contract added

---
+++ app/src/main/java/com/mmrbd/starwarsexplorer/presentation/home/contract/HomeContract.kt
@@ -0,0 +1,16 @@
+package com.mmrbd.starwarsexplorer.presentation.home.contract
+
+import com.mmrbd.starwarsexplorer.domain.entities.CharacterEntity
+import com.mmrbd.starwarsexplorer.utils.Result
+
+class HomeContract {
+    sealed class State {
+        data class GetCharacterList(val data: Result<List<CharacterEntity>>) : State()
+
+    }
+
+    sealed class Event {
+
+    }
+
+}
