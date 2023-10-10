ff4db30d9f63ddfc326b28f51bb2a731b04b90f9
Moklesur Rahman <moklesur.rahman@vegans.it>
2023-10-10 18:38:36 +0600

    test dependency and other dep added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.plugin.serialization")
     id("kotlin-parcelize")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -19,6 +20,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+
+        //build config
+        buildConfigField("String", "BASE_API_URL", "\"https://swapi.dev/api/\"")
     }
 
     buildTypes {
@@ -43,9 +48,10 @@ android {
     buildFeatures {
         viewBinding = true
     }
-    kapt {
-        correctErrorTypes = true
-    }
+}
+
+kapt {
+    correctErrorTypes = true
 }
 
 dependencies {
@@ -58,33 +64,52 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+
+    //Hilt
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
     implementation("androidx.fragment:fragment-ktx:1.6.1")
-
     debugImplementation("androidx.fragment:fragment-testing:1.6.1")
 
-
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.1")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+
 
 
     val room_version = "2.5.2"
 
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
+    testImplementation("androidx.room:room-testing:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
-
     implementation("androidx.room:room-ktx:$room_version")
 
+    //Gson
+    implementation("com.google.code.gson:gson:2.10.1")
 
+
+    //Test
+    androidTestImplementation ("androidx.arch.core:core-testing:2.2.0")
+    testImplementation ("androidx.arch.core:core-testing:2.2.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.robolectric:robolectric:4.9")
+    testImplementation("androidx.test:core-ktx:1.5.0")
+
+
+    //MockK
+    testImplementation ("io.mockk:mockk:1.12.0")
+    androidTestImplementation ("io.mockk:mockk-android:1.12.0")
+
+    testImplementation ("io.kotlintest:kotlintest-runner-junit5:3.3.2")
+    testImplementation ("com.squareup.okhttp3:mockwebserver:4.10.0")
 }
